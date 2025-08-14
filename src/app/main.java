cb4814559a61445faff9504739b0192cf08b4525
dulan-nimnaka <dulannimnaka123456@gmail.com>
2025-08-14 21:44:03 +0530

    added Main.java test class

--- src/app/main.java
+++ src/app/main.java
@@ -1,7 +1,7 @@
 package app;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Banking Application!");
+        System.out.println("Banking Application Starting...");
     }
 }
