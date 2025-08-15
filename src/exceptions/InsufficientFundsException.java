891f86bc94e61870061f2b1dcc583782980d4177
dulan-nimnaka <dulannimnaka123456@gmail.com>
2025-08-15 23:06:14 +0530

    added InsufficientFundsException.java

--- src/exceptions/InsufficientFundsException.java
+++ src/exceptions/InsufficientFundsException.java
@@ -1,5 +1,13 @@
 package exceptions;
 
-public class InsufficientFundsException {
-    
+// This is used to represent situations where there insufficient funds or overdraft
+public class InsufficientFundsException extends Exception{
+
+    // Constructor that takes a String parameter 'message' will describe the specific error that occurred
+    public InsufficientFundsException(String message) {
+
+        // Calls the parent class(Exception)
+        super(message);
+    }
 }
+
